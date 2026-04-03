8b08f403906222b91d9c0d9b33300f44fde8a353
Harshad Raj Gupta <gptharshad@gmail.com>
2026-04-03 14:02:29 +0530

    Ready for Deploy

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import random
 
+import os
 app = Flask(__name__)
 
 
@@ -130,3 +131,5 @@ def calculate():
 # 🔹 Run App
 if __name__ == "__main__":
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
